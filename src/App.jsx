2a829b3ae94994734fca01e88c7b02de29f0199c
Jon Mico <jonmico@gmail.com>
2023-09-07 11:19:39 -0400

    Make routes.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,34 @@
-export default function App() {
-  const x = 23;
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
-  return <div>hello vite</div>;
+import Home from './ui/Home';
+import Menu from './features/menu/Menu';
+import Cart from './features/cart/Cart';
+import CreateOrder from './features/order/CreateOrder';
+import Order from './features/order/Order';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+  },
+  {
+    path: '/menu',
+    element: <Menu />,
+  },
+  {
+    path: '/cart',
+    element: <Cart />,
+  },
+  {
+    path: '/order/new',
+    element: <CreateOrder />,
+  },
+  {
+    path: '/order/:orderId',
+    element: <Order />,
+  },
+]);
+
+export default function App() {
+  return <RouterProvider router={router} />;
 }
