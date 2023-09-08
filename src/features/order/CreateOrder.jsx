3416f3372ef7b81e729944da78eab1cdc19bb52e
Jon Mico <jonmico@gmail.com>
2023-09-08 15:08:17 -0400

    Add feedback to Place Order button.

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Form, redirect } from 'react-router-dom';
+import { Form, redirect, useNavigation } from 'react-router-dom';
 import { createOrder } from '../../services/apiRestaurant';
 
 // https://uibakery.io/regex-library/phone-number
@@ -49,6 +49,8 @@ export async function action({ request }) {
 
 function CreateOrder() {
   // const [withPriority, setWithPriority] = useState(false);
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === 'submitting';
   const cart = fakeCart;
 
   return (
@@ -88,7 +90,9 @@ function CreateOrder() {
 
         <div>
           <input type={'hidden'} name={'cart'} value={JSON.stringify(cart)} />
-          <button>Order now</button>
+          <button disabled={isSubmitting}>
+            {isSubmitting ? 'Placing order...' : 'Order now'}
+          </button>
         </div>
       </Form>
     </div>
