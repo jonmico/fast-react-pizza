c7c8f1efd304adb951cec0b0e3b485b0b5b37685
Jon Mico <jonmico@gmail.com>
2023-09-15 12:21:12 -0400

    Add some form styling.

--- src/features/order/SearchOrder.jsx
+++ src/features/order/SearchOrder.jsx
@@ -20,6 +20,9 @@ function SearchOrder() {
         placeholder={'Search order #'}
         value={query}
         onChange={(evt) => setQuery(evt.target.value)}
+        className={
+          'rounded-full px-4 py-2 text-sm bg-yellow-100 placeholder:text-stone-400 w-28 sm:w-64 sm:focus:w-72 transition-all duration-300 focus:outline-none focus:ring focus:ring-yellow-500 focus:ring-opacity-50'
+        }
       />
     </form>
   );
