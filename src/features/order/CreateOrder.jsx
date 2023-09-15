6c26c2e229fd823d591fc918168df533b8b4c300
Jon Mico <jonmico@gmail.com>
2023-09-14 23:21:48 -0400

    Add some styling for a button.

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -100,7 +100,12 @@ function CreateOrder() {
 
         <div>
           <input type={'hidden'} name={'cart'} value={JSON.stringify(cart)} />
-          <button disabled={isSubmitting}>
+          <button
+            className={
+              'bg-yellow-400 uppercase tracking-wide font-semibold text-stone-800 py-3 px-4 inline-block rounded-full hover:bg-yellow-300 transition-colors duration-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:bg-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed'
+            }
+            disabled={isSubmitting}
+          >
             {isSubmitting ? 'Placing order...' : 'Order now'}
           </button>
         </div>
