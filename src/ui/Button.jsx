3d3e79c9463c9b2518bd6da8ce222d1e2c0e51e6
Jon Mico <jonmico@gmail.com>
2023-09-25 12:29:33 -0400

    Implement UpdateItemQuantity.

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -6,7 +6,8 @@ export default function Button({ children, disabled, to, type, onClick }) {
 
   const styles = {
     primary: base + ' py-3 px-4 md:px-6 md:py-4',
-    small: base + ' px-2 py-2 md:px-5 md:py-2.5 text-xs',
+    small: base + ' px-4 py-2 md:px-5 md:py-2.5 text-xs',
+    round: base + ' px-2.5 py-1 md:px-3.5 md:py-2 text-sm',
     secondary:
       'text-sm border-2 border-stone-300 uppercase tracking-wide font-semibold text-stone-400 inline-block rounded-full hover:bg-stone-300 hover:text-stone-800 transition-colors duration-300 focus:outline-none focus:ring focus:text-stone-800 focus:ring-stone-200 focus:bg-stone-300 focus:ring-offset-2 disabled:cursor-not-allowed py-2.5 px-4 md:px-6 md:py-3.5',
   };
